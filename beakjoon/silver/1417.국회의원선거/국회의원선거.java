4b997a1f5767bc032a7c2214abe550e6d173b18d
김나경 <1030n@naver.com>
2025-07-09 16:26:46 +0900

    :sparkles: 1417 국회의원 선거

--- beakjoon/silver/1417.국회의원선거/국회의원선거.java
+++ beakjoon/silver/1417.국회의원선거/국회의원선거.java
@@ -4,41 +4,43 @@ import java.util.*;
 public class Main {
   static int n;
   static int num[];
-  static int result;
+  static int result = 0;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     n = Integer.parseInt(br.readLine());
 
     num = new int[n];
-    result = 0;
+    for (int i = 0; i < n; i++)
+      num[i] = Integer.parseInt(br.readLine());
 
-    for (int i = 0; i < n; i++) 
-        num[i] = Integer.parseInt(br.readLine());
+    if (n == 1) {
+      System.out.println(0);
+      return;
+    }
 
-    // 가장 투표 수가 많은 사람의 표를 1표씩 가져온다.
     while (true) {
-        int winPerson = still();
-        if (num[0] > num[winPerson])
-            break;
+      int maxIdx = -1;
+      int maxVote = -1;
+
+      // 1등 찾기
+      for (int i = 1; i < n; i++) {
+        if (num[i] > maxVote) {
+          maxVote = num[i];
+          maxIdx = i;
+        }
+      }
+
+      // [0]이 1등이면 종료
+      if (num[0] > maxVote)
+        break;
+
+      // 1등 표 가져오기
+      num[maxIdx]--;
+      num[0]++;
+      result++;
     }
-      System.out.println(result);
-  }
 
-  // 가장 많이 투표를 받은 사람 번호 반환
-  public static int still() {
-    int winPerson = 1;
-    // 가장 많은 투표를 받은 사람을 찾는다.
-    for (int i = 1; i < n; i++) {
-        if (num[winPerson] < num[i])
-            winPerson = i;
-    }
-
-    // 해당 사람의 투표를 1개 뺏고 [0]에게 추가
-    num[winPerson] -= 1;
-    num[0] += 1;
-    result += 1;
-
-    return winPerson;
+    System.out.println(result);
   }
 }

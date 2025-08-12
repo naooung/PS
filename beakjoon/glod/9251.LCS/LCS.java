b3aed27d6fece85c24e5abb7765ed1cdfabc7337
김나경 <1030n@naver.com>
2025-08-13 04:03:46 +0900

    :sparkles: 9251 LCS

---
+++ beakjoon/glod/9251.LCS/LCS.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int n = s1.length();
+        int m = s2.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // 같으면 전거에서 +1 -> 전게 아니라 대각선에서 +1!!!
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                // 위랑 옆 중에 큰걸로 가져옴
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println((dp[n][m]));
+    }
+}
